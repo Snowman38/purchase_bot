d818ddd724d0fe730e09bb4094fae85b91b12775
Victor <victor.lecardonnel@hotmail.fr>
2021-05-05 17:06:41 +0200

    started login flow

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
 import requests
+from bs4 import BeautifulSoup
+import sys
 import time
 
 s = requests.session()
 
+headers = {
+    'Connection': 'keep-alive',
+    'Cache-Control': 'max-age=0',
+    'sec-ch-ua': '^\\^',
+    'sec-ch-ua-mobile': '?0',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-User': '?1',
+    'Sec-Fetch-Dest': 'document',
+    'Referer': 'https://www.pokemoncenter-online.com/?main_page=shopping_cart',
+    'Accept-Language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
+    'dnt': '1',
+}
+
+params = (
+    ('main_page', 'login'),
+)
+
+g = s.get('https://www.pokemoncenter-online.com/',
+          params=params, headers=headers)
+
+soup = BeautifulSoup(g.text, 'html.parser')
+
+print(soup.prettify().encode('utf-8'))
+
+
+"""
 headers = {
     'Connection': 'keep-alive',
     'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
@@ -33,3 +65,4 @@ while true:
     else:
 
 print(response.cookies)
+"""
